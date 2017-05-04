bf6284c680316e82941903d830e0a767e6e48aa3
田径 <tian___jing@hotmail.com>
2017-05-04 16:30:23 +0800

    增加转换方法

--- QHandlers/util.h
+++ QHandlers/util.h
@@ -24,7 +24,19 @@ public:
 		}
 	}
 
-
+	static void CopyMap(QMap<QString, QString>& p_SourceMap,std::multimap<CefString, CefString>& p_TargetMap)
+	{
+		QMap<QString, QString>::iterator begin, end;
+		begin = p_SourceMap.begin();
+		end = p_SourceMap.end();
+		while (begin != end)
+		{
+			CefString key= QCefUtil::ToString(begin.key());
+			CefString value= QCefUtil::ToString(begin.value());
+			p_TargetMap.insert(std::pair<CefString, CefString>(key, value));
+			++begin;
+		}
+	}
 	static void CopyList(std::vector<QString>& p_Source, std::vector<CefString>& p_Target)
 	{
 		size_t size = p_Source.size();
